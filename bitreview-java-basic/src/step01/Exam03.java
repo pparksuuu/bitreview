38d326c4bb596fdc93ac461ef48ea06410d4d403
pparksuuu <supr2000@gmail.com>
2018-02-28 20:57:48 +0900

    '.'

---
+++ bitreview-java-basic/src/step01/Exam03.java
@@ -0,0 +1,8 @@
+package step01;
+public class Exam03 {
+    static String message = "Hello!";
+    public static void main(String[] args) {
+        System.out.println("You're in step01/Exam03");
+        System.out.println(message);
+    }
+}
