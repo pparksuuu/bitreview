8cd865311f1325c49f60b90a893b41e0f1a2f5fc
pparksuuu <supr2000@gmail.com>
2018-03-15 11:14:09 +0900

    '18.03.15'

---
+++ bitreview-java-basic/src/step06/Exam02_1.java
@@ -0,0 +1,16 @@
+//메서드 : 개념 및 기본문법
+package step06;
+
+import java.util.Scanner;
+
+public class Exam02_1 {
+    static void hello() {
+        System.out.println("안녕하세요");
+        System.out.println("이 메서드는 어떤 값도 리턴하지 않습니다.");
+    }
+
+    public static void main(String[] args) {
+        hello();
+        
+    }
+}
