b6dce2462295da7b64d5af31b1fafb78a5352bba
pparksuuu <supr2000@gmail.com>
2018-05-18 17:53:37 +0900

    '18.05.18'

--- bitreview-java-project/src/main/java/bitcamp/java106/pms/dao/TeamMemberDao.java
+++ bitreview-java-project/src/main/java/bitcamp/java106/pms/dao/TeamMemberDao.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Component;
 
+import bitcamp.java106.pms.domain.Member;
+
 @Component
 public class TeamMemberDao {
 
@@ -29,6 +31,18 @@ public class TeamMemberDao {
         }
     }
     
+    public int delete(String teamName) throws Exception {
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            HashMap<String,Object> paramMap = new HashMap<>();
+            paramMap.put("teamName", teamName);
+            
+            int count = sqlSession.delete(
+                    "bitcamp.java106.pms.dao.TeamMemberDao.delete", paramMap);
+            sqlSession.commit();
+            return count;
+        } 
+    }
+    
     public int delete(String teamName, String memberId) throws Exception {
         try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
             HashMap<String,Object> paramMap = new HashMap<>();
@@ -46,6 +60,13 @@ public class TeamMemberDao {
         try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
             return sqlSession.selectList(
                     "bitcamp.java106.pms.dao.TeamMemberDao.selectList", teamName);
+        }
+    }
+    
+    public List<Member> selectListWithEmail(String teamName) throws Exception {
+        try (SqlSession sqlSession = this.sqlSessionFactory.openSession()) {
+            return sqlSession.selectList(
+                    "bitcamp.java106.pms.dao.TeamMemberDao.selectListWithEmail", teamName);
         }
     }
     
