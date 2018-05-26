0538386c6ee75a56458800af9f8f439b04d49a11
pparksuuu <supr2000@gmail.com>
2018-05-26 22:27:11 +0900

    src09 ~ 14 practice

---
+++ bitreview-java-project/src14/main/java/bitcamp/java106/pms/domain/Team.java
@@ -0,0 +1,11 @@
+package bitcamp.java106.pms.domain;
+
+import java.sql.Date;
+
+public class Team {
+    public String name;
+    public String description;
+    public int maxQty;
+    public Date startDate;
+    public Date endDate;
+}
