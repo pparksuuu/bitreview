4e3f30c6257499a5409187be744047f5c07d6b85
pparksuuu <supr2000@gmail.com>
2018-05-30 00:24:49 +0900

    '18.05.29'

--- bitreview-java-project/src46/main/java/bitcamp/java106/pms/servlet/DispatcherServlet.java
+++ bitreview-java-project/src46/main/java/bitcamp/java106/pms/servlet/DispatcherServlet.java
@@ -2,7 +2,6 @@
 package bitcamp.java106.pms.servlet;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,38 +11,38 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import bitcamp.java106.pms.controller.PageController;
 import bitcamp.java106.pms.support.WebApplicationContextUtils;
-import bitcamp.java106.pms.web.RequestMapping;
 
 @SuppressWarnings("serial")
 public class DispatcherServlet extends HttpServlet {
-
+    
     ApplicationContext iocContainer;
-
+    
     @Override
     public void init() throws ServletException {
         // 이 서블릿을 생성할 때 이 서블릿이 사용할 Spring IoC 컨테이너를 준비한다.
         try {
             iocContainer = new ClassPathXmlApplicationContext(
-                    this.getServletConfig().getInitParameter("contextConfigLocation"));
-
+                this.getServletConfig().getInitParameter("contextConfigLocation"));
+            
             // 다른 서블릿에서 스프링 IoC 컨테이너를 꺼내 쓸 수 있도록,
             // WebApplicationContextUtils에 보관한다.
             WebApplicationContextUtils.containers.put(
                     this.getServletContext(), iocContainer);
-
+            
             String[] beanNames = iocContainer.getBeanDefinitionNames();
             System.out.println("-----------------------------");
             for (String name : beanNames) {
                 System.out.println(name);
             }
             System.out.println("-----------------------------");
-
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    
     @Override
     protected void service(
             HttpServletRequest request, 
@@ -52,24 +51,18 @@ public class DispatcherServlet extends HttpServlet {
         // => 클라이언트의 모든 요청을 받아서 특정 컨트롤러에게 위임하기 전에 
         //    필요한 공통 기능 처리한다.
         // => 클라이언트의 진입점이 한 군데라서 유지보수가 용이하다.
-
+        
         response.setContentType("text/html;charset=UTF-8");
 
         // 클라이언트가 요청한 서블릿의 경로를 알아내기
         String servletPath = request.getServletPath().replace(".do", "");
-
+         
+        // 클라이언트 요청을 처리할 페이지 컨트롤러를 얻기
+        PageController pageController = (PageController)iocContainer.getBean(servletPath);
+        
+        // 페이지 컨트롤러 실행
         try {
-            // 클라이언트 요청을 처리할 페이지 컨트롤러를 얻기
-            Object pageController = iocContainer.getBean(servletPath);
-
-            Method requestHandler = findRequestHandler(pageController);
-
-            if (requestHandler == null) 
-                throw new ServletException("요청을 처리할 요청 핸들러가 없습니다");
-
-            String viewUrl = (String) requestHandler.invoke(
-                    pageController, request, response);
-
+            String viewUrl = pageController.service(request, response);
             if (viewUrl.startsWith("redirect:")) {
                 response.sendRedirect(viewUrl.substring(9));
             } else {
@@ -78,17 +71,6 @@ public class DispatcherServlet extends HttpServlet {
         } catch (Exception e) {
             throw new ServletException("페이지 컨트롤러 실행 중 오류 발생!");
         }
-    }
-
-    private Method findRequestHandler(Object pageController) throws Exception{
-        Class<?> clazz = pageController.getClass();
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method m : methods) {
-            RequestMapping anno = m.getAnnotation(RequestMapping.class);
-            if (anno != null)
-                return m;
-        }
-        return null;
     }
 }
 
