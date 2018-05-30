1c3ec90f4e07efaee8f2a08b3b3971e39703b4c5
pparksuuu <supr2000@gmail.com>
2018-05-30 21:29:01 +0900

    '18.05.30'

--- bitreview-java-project/src/main/java/bitcamp/java106/pms/web/RequestParam.java
+++ bitreview-java-project/src/main/java/bitcamp/java106/pms/web/RequestParam.java
@@ -4,6 +4,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RequestMapping {
+public @interface RequestParam {
     String value() default "";
 }
