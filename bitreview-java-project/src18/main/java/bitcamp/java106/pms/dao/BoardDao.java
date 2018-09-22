91ca8f0e8c0d4b5104be36af2613338c27f28b24
pparksuuu <supr2000@gmail.com>
2018-09-22 19:13:06 +0900

    [복습]src18-컬랙션 클래스 제공

---
+++ bitreview-java-project/src18/main/java/bitcamp/java106/pms/dao/BoardDao.java
@@ -0,0 +1,52 @@
+package bitcamp.java106.pms.dao;
+
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.util.ArrayList;
+
+public class BoardDao {
+    private ArrayList collection = new ArrayList();
+    
+    public void insert(Board board) {
+        this.collection.add(board);
+    }
+    
+    public Board[] list() {
+        Board[] arr = new Board[this.collection.size()];
+        for (int i = 0; i < this.collection.size(); i++) {
+            arr[i] = (Board) this.collection.get(i);
+        }
+        return arr;
+    }
+    
+    public Board get(int no) {
+        int index = this.getBoardIndex(no);
+        if (index < 0)
+            return null;
+        return (Board) collection.get(index);
+    }
+    
+    public void update(Board board) {
+        int index = this.getBoardIndex(board.getNo());
+        if (index < 0)
+            return;
+        collection.set(index, board);
+    }
+    
+    public void delete(int no) {
+        int index = this.getBoardIndex(no);
+        if (index < 0)
+            return;
+        collection.remove(index);
+    }
+    
+    private int getBoardIndex(int no) {
+        for (int i = 0; i < collection.size(); i++) {
+            Board originBoard = 
+                    (Board)collection.get(i);
+            if (originBoard.getNo() == no) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
