fbf4c23c59c77e775dd6d7c3cb8981151eb2e3ef
pparksuuu <supr2000@gmail.com>
2018-09-22 20:00:30 +0900

    [복습]src20 - 수업 정보를 관리할 수 있는 CRUD 추가

--- bitreview-java-project/src/main/java/bitcamp/java106/pms/App.java
+++ bitreview-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -5,10 +5,12 @@ import java.sql.Date;
 import java.util.Scanner;
 
 import bitcamp.java106.pms.controller.BoardController;
+import bitcamp.java106.pms.controller.ClassroomController;
 import bitcamp.java106.pms.controller.MemberController;
 import bitcamp.java106.pms.controller.TaskController;
 import bitcamp.java106.pms.controller.TeamController;
 import bitcamp.java106.pms.controller.TeamMemberController;
+import bitcamp.java106.pms.dao.ClassroomDao;
 import bitcamp.java106.pms.dao.MemberDao;
 import bitcamp.java106.pms.dao.TaskDao;
 import bitcamp.java106.pms.dao.TeamDao;
@@ -43,6 +45,7 @@ public class App {
         MemberDao memberDao = new MemberDao();
         TaskDao taskDao = new TaskDao();
         TeamMemberDao teamMemberDao = new TeamMemberDao();
+        ClassroomDao classroomDao = new ClassroomDao();
         
         // 테스트용 데이터 준비
         prepareMemberData(memberDao);
@@ -54,6 +57,7 @@ public class App {
         BoardController boardController = new BoardController(keyScan);
         TaskController taskController = new TaskController(
                 keyScan, teamDao, taskDao, teamMemberDao, memberDao);
+        ClassroomController classroomController = new ClassroomController(keyScan);
         
         Console.keyScan = keyScan;
         
@@ -82,6 +86,8 @@ public class App {
                 boardController.service(menu, option);
             } else if (menu.startsWith("task/")) {
                taskController.service(menu, option); 
+            } else if (menu.startsWith("classroom/")) {
+                classroomController.service(menu, option);
             } else {
                 System.out.println("명령어가 올바르지 않습니다.");
             }
