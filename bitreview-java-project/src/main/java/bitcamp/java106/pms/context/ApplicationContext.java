dd40a629311db4ff39c24a5c17e53c70cbca8010
pparksuuu <supr2000@gmail.com>
2018-09-23 23:39:15 +0900

    [복습]src23 - IoC컨테이너를 적용하라!

---
+++ bitreview-java-project/src/main/java/bitcamp/java106/pms/context/ApplicationContext.java
@@ -0,0 +1,17 @@
+package bitcamp.java106.pms.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private HashMap<String, Object> objPool = new HashMap<>();
+    
+    public ApplicationContext(String packageName, Map<String, Object> beans) throws Exception {
+        // 다른 맵에서 들어있는 객체를 이 컨테이너에 복사한다.
+        if (beans != null) {
+            objPool.putAll(beans);
+        }
+        
+        
+    }
+}
