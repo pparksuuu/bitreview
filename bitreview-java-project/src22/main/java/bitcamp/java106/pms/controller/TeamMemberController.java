34142e8d028c8f2f92e512f680ed7272bbc52bf1
pparksuuu <supr2000@gmail.com>
2018-09-23 23:14:19 +0900

    [복습]src22 - DAO에 대해 generalization을 적용하라!

---
+++ bitreview-java-project/src22/main/java/bitcamp/java106/pms/controller/TeamMemberController.java
@@ -0,0 +1,116 @@
+package bitcamp.java106.pms.controller;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+import bitcamp.java106.pms.dao.MemberDao;
+import bitcamp.java106.pms.dao.TeamDao;
+import bitcamp.java106.pms.dao.TeamMemberDao;
+import bitcamp.java106.pms.domain.Member;
+import bitcamp.java106.pms.domain.Team;
+
+public class TeamMemberController implements Controller {
+    Scanner keyScan;
+    TeamDao teamDao;
+    MemberDao memberDao;
+    TeamMemberDao teamMemberDao;
+    
+    public TeamMemberController(Scanner scanner, TeamDao teamDao, MemberDao memberDao, TeamMemberDao teamMemberDao) {
+       this.keyScan = scanner;
+       this.teamDao = teamDao;
+       this.memberDao = memberDao;
+       this.teamMemberDao = teamMemberDao;
+    }
+    
+    public void service(String menu, String option) {
+        if (menu.equals("team/member/add")) {
+            this.onTeamMemberAdd(option);
+        } else if (menu.equals("team/member/list")) {
+            this.onTeamMemberList(option);
+        } else if (menu.equals("team/member/delete")) {
+            this.onTeamMemberDelete(option);
+        } else {
+            System.out.println("명령어가 올바르지 않습니다");
+        }
+    }
+    
+    void onTeamMemberAdd(String teamName) {
+        if (teamName == null) {
+            System.out.println("팀명을 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Team team = teamDao.get(teamName);
+        if (team == null) {
+            System.out.printf("%s 팀은 존재하지 않습니다.", teamName);
+            return;
+        }
+        
+        System.out.println("[팀 멤버 추가]");
+        System.out.print("추가할 멤버의 아이디는?");
+        String memberId = keyScan.nextLine();
+        
+        Member member = memberDao.get(memberId);
+        if (member == null) {
+            System.out.printf("%s 회원은 없습니다.", memberId);
+            return;
+        }
+        
+        if (team.isExist(memberId)) {
+            System.out.println("이미 등록된 회원입니다.");
+            return;
+        }
+        
+        team.addMember(member);
+    }
+    
+    void onTeamMemberList(String teamName) {
+        if (teamName == null) {
+            System.out.println("팀명을 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Team team = teamDao.get(teamName);
+        if (team == null) {
+            System.out.printf("%s 팀은 존재하지 않습니다.", teamName);
+            return;
+        }
+        
+        System.out.println("[팀 멤버 목록]");
+        System.out.print("회원들 : ");
+        
+        Iterator<String> members = teamMemberDao.getMembers(teamName);
+        
+        for (int i = 0; i < team.getMembers().length; i++) {
+            if (team.getMembers()[i] == null) continue;
+            System.out.printf("%s, ", team.getMembers()[i].getId());
+        }
+        System.out.println();
+    }
+    
+    void onTeamMemberDelete(String teamName) {
+        if (teamName == null) {
+            System.out.println("팀명을 입력하시기 바랍니다.");
+            return;
+        }
+        
+        Team team = teamDao.get(teamName);
+        if (team == null) {
+            System.out.printf("%s 팀은 존재하지 않습니다.", teamName);
+            return;
+        }
+        
+        System.out.print("삭제할 팀원은? ");
+        String memberId = keyScan.nextLine();
+        
+        if (!team.isExist(memberId)) {
+            System.out.println("이 팀의 회원이 아닙니다.");
+            return;
+        }
+        
+        team.deleteMember(memberId);
+        
+        System.out.println("[팀 멤버 삭제]");
+        System.out.println("삭제하였습니다.");
+    }
+}
