b3035a33723a6e9cde60f18ab60c563cba4a007b
pparksuuu <supr2000@gmail.com>
2019-04-04 18:00:48 +0900

    .

---
+++ bitreview-spring-ioc/src/main/java/bitcamp/java106/step08_1/Exam02.java
@@ -0,0 +1,14 @@
+package bitcamp.java106.step08_1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam02 {
+	public static void main(String[] args) {
+		ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+				"bitcamp/java106/step08_1/application-context-01.xml");
+		
+		// Car 객체를 꺼내 Engine 객체가 주입되었는지 확인해보자 !
+		System.out.println(iocContainer.getBean("c1"));
+	}
+}
